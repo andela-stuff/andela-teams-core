d6b8bd59c5f014b58623b26011117ee42763c0b1
Franklin Chieze <chieze.franklin@gmail.com>
2018-01-23 03:52:17 +0100

    remove console.log from /models/index.js

--- src/models/index.js
+++ src/models/index.js
@@ -30,11 +30,4 @@ Object.keys(db).forEach((modelName) => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => console.error('Unable to connect to the database:', err));
-
 export default db;

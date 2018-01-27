c51435cac0d5759c694aa2d881970589d69f9e9a
Franklin Chieze <chieze.franklin@gmail.com>
2018-01-27 16:11:57 +0100

    add User model

---
+++ src/models/user.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  });
+  return User;
+};

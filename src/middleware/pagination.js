db685764db8489530ca1affd26b0298f264beaf8
Franklin Chieze <chieze.franklin@gmail.com>
2018-02-06 23:35:56 +0100

    add JsDoc to /middleware/pagination.js

--- src/middleware/pagination.js
+++ src/middleware/pagination.js
@@ -4,6 +4,15 @@
  * @author Franklin Chieze
  */
 
+/**
+ * @desc This middleware adds pagination metadata to the request object
+ *
+ * @param { object } req request
+ * @param { object } res response
+ * @param { object } next the next middleware or endpoint
+ *
+ * @returns { object } next
+ */
 export default (req, res, next) => {
   req.meta = req.meta || {};
   req.meta.pagination = req.meta.pagination || { page: 1 };

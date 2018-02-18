b1355f94d50672d4ff306a81accf4595c3c2f7cb
Franklin Chieze <chieze.franklin@gmail.com>
2018-02-18 12:04:28 +0100

    add basic tests for Auth controller

--- test/controllers/auth.spec.js
+++ test/controllers/auth.spec.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt';
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 
@@ -14,13 +15,28 @@ describe('AuthController', () => {
     await models.User.destroy({ where: {}, truncate: true });
   });
   describe('POST: /v1/auth/signin', (done) => {
-    it('should respond with an array', (done) => {
+    beforeEach(async () => {
+      models.User.create({
+        name: mock.user1.name,
+        email: mock.user1.email,
+        password: await bcrypt.hash(mock.password, 1)
+      });
+    });
+    it('should return a user token on successful sign in', (done) => {
       chai.request(server)
-        .get('/v1/users')
+        .post('/v1/auth/signin')
+        .send(mock.user1)
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.have.property('data');
-          expect(res.body.data).to.be.an('Array');
+          expect(res.body.data).to.be.an('Object');
+          res.body.data.should.have.property('user');
+          expect(res.body.data.user.id).to.not.be.undefined;
+          expect(res.body.data.user.name).to.equal(mock.user1.name);
+          expect(res.body.data.user.email).to.equal(mock.user1.email);
+          res.body.data.user.should.not.have.property('password');
+          res.body.data.should.have.property('userToken');
+          expect(res.body.errors).to.be.undefined;
           done();
         });
     });

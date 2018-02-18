1072c8cf7078f46ce4718a4056e6d9bb4ba8d825
Franklin Chieze <chieze.franklin@gmail.com>
2018-02-18 10:52:28 +0100

    add /test/mock to hold mock data

---
+++ test/controllers/auth.spec.js
@@ -0,0 +1,49 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+
+import mock from '../mock';
+import models from '../../build/models';
+import server from '../../build/server';
+
+const should = chai.should();
+const { expect } = chai;
+chai.use(chaiHttp);
+
+describe('AuthController', () => {
+  beforeEach(async () => {
+    await models.User.destroy({ where: {}, truncate: true });
+  });
+  describe('POST: /v1/auth/signin', (done) => {
+    it('should respond with an array', (done) => {
+      chai.request(server)
+        .get('/v1/users')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('data');
+          expect(res.body.data).to.be.an('Array');
+          done();
+        });
+    });
+  });
+
+  describe('POST: /v1/auth/signup', (done) => {
+    it('should register a new user', (done) => {
+      chai.request(server)
+        .post('/v1/auth/signup')
+        .send(mock.user1)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('data');
+          expect(res.body.data).to.be.an('Object');
+          res.body.data.should.have.property('user');
+          expect(res.body.data.user.id).to.not.be.undefined;
+          expect(res.body.data.user.name).to.equal(mock.user1.name);
+          expect(res.body.data.user.email).to.equal(mock.user1.email);
+          res.body.data.user.should.not.have.property('password');
+          res.body.data.should.have.property('userToken');
+          expect(res.body.errors).to.be.undefined;
+          done();
+        });
+    });
+  });
+});

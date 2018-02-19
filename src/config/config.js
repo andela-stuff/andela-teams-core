96406a85dd6f1c35f9877a7a32e3632495cb2d94
Franklin Chieze <chieze.franklin@gmail.com>
2018-02-19 17:28:33 +0100

    get sequelize working on Travis and Circle CI (take 3)

--- src/config/config.js
+++ src/config/config.js
@@ -5,6 +5,7 @@ module.exports = {
     database: process.env.DATABASE_DEV,
     dialect: 'postgres',
     host: '127.0.0.1',
+    operatorsAliases: false,
     password: process.env.DATABASE_DEV_PASSWORD,
     username: process.env.DATABASE_DEV_USERNAME,
     url: process.env.DATABASE_DEV_URL
@@ -13,13 +14,15 @@ module.exports = {
     database: process.env.DATABASE_TEST,
     dialect: 'postgres',
     host: '127.0.0.1',
+    operatorsAliases: false,
     password: process.env.DATABASE_TEST_PASSWORD,
     username: process.env.DATABASE_TEST_USERNAME,
     url: process.env.DATABASE_TEST_URL
   },
   production: {
-    url: process.env.DATABASE_URL,
     dialect: 'postgres',
+    url: process.env.DATABASE_URL,
+    operatorsAliases: false,
   },
   secret: process.env.SECRET,
 };

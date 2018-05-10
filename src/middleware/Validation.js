a36687f5bdef0621e78c5d303b5383cf7e883408
Franklin Chieze <chieze.franklin@gmail.com>
2018-05-10 18:03:44 +0100

    remove 'teamId' from body of POST to /v1/teams/:teamId/members

--- src/middleware/Validation.js
+++ src/middleware/Validation.js
@@ -31,7 +31,6 @@ const createTeamRules = {
 };
 const createTeamMemberRules = {
   role: 'string',
-  teamId: 'required|string',
   userId: 'required|string',
 };
 
@@ -112,7 +111,6 @@ export default class Validation {
     const validation = new Validator(req.body, createTeamMemberRules);
     validation.fails(() => res.sendFailure([
       ...validation.errors.get('role'),
-      ...validation.errors.get('teamId'),
       ...validation.errors.get('userId'),
     ]));
     validation.passes(() => next());

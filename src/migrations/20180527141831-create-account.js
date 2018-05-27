9bdf38e3dc6ec69c85fde39bf64f8137226997df
Franklin Chieze <chieze.franklin@gmail.com>
2018-05-27 23:58:45 +0100

    add migration file for account

---
+++ src/migrations/20180527141831-create-account.js
@@ -0,0 +1,54 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('Memberships', {
+      id: {
+        allowNull: false,
+        // autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      role: {
+        type: Sequelize.ENUM,
+        values: [
+          'disabled', 'developer', 'lead', 'member'
+        ],
+        defaultValue: 'member'
+      },
+      teamId: {
+        type: Sequelize.UUID,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Teams',
+          key: 'id',
+          as: 'teamId',
+        },
+      },
+      userId: {
+        type: Sequelize.UUID,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Users',
+          key: 'id',
+          as: 'userId',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    }),
+  down: (queryInterface, Sequelize) => {
+    const result = queryInterface.dropTable('Memberships');
+
+    // manually drop "enum_Memberships_role" since Sequelize does not
+    // drop when dropping "Memberships"
+    queryInterface.sequelize.query('DROP TYPE "enum_Memberships_role";');
+
+    return result;
+  }
+};
